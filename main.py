834212f925913545f076f5202ce045ea555ba716
Ritus0000 <Den_ritus@icloud.com>
2025-11-30 20:55:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # main.py
 #
 # Точка входа для Railway.
-# Просто импортирует и запускает main() из training_scheduler_bot.
+# Просто вызывает main() из training_scheduler_bot.
 
 from training_scheduler_bot import main as run_training_scheduler_bot
 

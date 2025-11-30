acd47570a59bf4dcfff3c51f2e9a11924e6d6c51
Ritus0000 <Den_ritus@icloud.com>
2025-11-30 20:40:07 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+#
+# Точка входа для Railway.
+# Просто импортирует и запускает main() из training_scheduler_bot.
+
+from training_scheduler_bot import main as run_training_scheduler_bot
+
+
+if __name__ == "__main__":
+    run_training_scheduler_bot()
